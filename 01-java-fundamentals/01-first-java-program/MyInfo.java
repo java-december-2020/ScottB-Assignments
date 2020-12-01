8ad4995fdfe54a4efa2b752745d4b467d91a3403
Scott Bagley <scott@base-four.com>
2020-12-01 14:11:56 -0600

    Added first program and pythagorean program

---
+++ 01-java-fundamentals/01-first-java-program/MyInfo.java
@@ -0,0 +1,7 @@
+public class MyInfo {
+    public static void main(String[] args) {
+        System.out.println("My name is Scott Bagley");
+        System.out.println("I am 46 years old");
+        System.out.println("My hometown is Dallas, TX");
+    }
+}
