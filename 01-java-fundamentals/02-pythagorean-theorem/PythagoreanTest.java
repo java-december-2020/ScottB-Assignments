c88427465d963d4cd62668134cc875778fe11921
scott-bagley <72776589+scott-bagley@users.noreply.github.com>
2020-12-01 14:09:14 -0600

    Delete PythagoreanTest.java

--- 01-java-fundamentals/02-pythagorean-theorem/PythagoreanTest.java
+++
@@ -1,7 +0,0 @@
-public class PythagoreanTest {
-    public static void main(String[] args) {
-        Pythagorean iD = new Pythagorean();
-        double legC = iD.calculateHypotenuse(3,4);
-        System.out.println(legC);
-    }
-}
