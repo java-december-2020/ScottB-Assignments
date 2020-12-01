415f4a7ae0e5d6d7773adddf3a4f307566ce8ecb
scott-bagley <72776589+scott-bagley@users.noreply.github.com>
2020-12-01 14:08:54 -0600

    Delete MyInfo.java

--- 01-java-fundamentals/01-first-java-program/MyInfo.java
+++
@@ -1,7 +0,0 @@
-public class MyInfo {
-    public static void main(String[] args) {
-        System.out.println("My name is Scott Bagley");
-        System.out.println("I am 46 years old");
-        System.out.println("My hometown is Dallas, TX");
-    }
-}
