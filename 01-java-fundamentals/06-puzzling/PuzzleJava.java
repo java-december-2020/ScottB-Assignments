813b6e230060c0398000108bb608ceb993773b6d
Scott Bagley <scott@base-four.com>
2020-12-06 19:53:46 -0600

    Puzzling

--- 01-java-fundamentals/06-puzzling/PuzzleJava.java
+++ 01-java-fundamentals/06-puzzling/PuzzleJava.java
@@ -3,7 +3,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Random;
 
-
 public class PuzzleJava {
     public ArrayList<Integer> sumArray(int[] myArray) {
         ArrayList<Integer> newArray = new ArrayList<Integer>();
@@ -16,52 +15,53 @@ public class PuzzleJava {
             }
         }
         System.out.println("Array Sum is : " + arraySum);
-        return(newArray);
+        return (newArray);
     }
 
     public ArrayList<String> shuffleNames(ArrayList<String> myArray) {
         ArrayList<String> newArray = new ArrayList<String>();
 
         Collections.shuffle(myArray);
-        for (int i=0; i < myArray.size(); i++) {
+        for (int i = 0; i < myArray.size(); i++) {
             System.out.println(myArray.get(i));
             if (myArray.get(i).length() > 5) {
                 newArray.add(myArray.get(i));
             }
         }
 
-        return(newArray);
+        return (newArray);
     }
 
     public void shuffleLetters() {
-         //create an array of letters in the alphabet
-         ArrayList<String> letters = new ArrayList<String>();
-         for(int i = 0; i < 26; i++){
-             letters.add(Character.toString((char)(65 + i)));
-         }
+        // create an array of letters in the alphabet
+        ArrayList<String> letters = new ArrayList<String>();
+        for (int i = 0; i < 26; i++) {
+            letters.add(Character.toString((char) (65 + i)));
+        }
 
-         Collections.shuffle(letters);
-         System.out.println(letters);
-         System.out.println(letters.get(0));
-         System.out.println(letters.get(25));
-         if (letters.get(0).equals("A") || letters.get(0).equals("E") || letters.get(0).equals("I") || letters.get(0).equals("O") || letters.get(0).equals("U")) {
-             System.out.println(letters.get(0) + " is a vowel");
-         }
-         
+        Collections.shuffle(letters);
+        System.out.println(letters);
+        System.out.println(letters.get(0));
+        System.out.println(letters.get(25));
+        if (letters.get(0).equals("A") || letters.get(0).equals("E") || letters.get(0).equals("I")
+                || letters.get(0).equals("O") || letters.get(0).equals("U")) {
+            System.out.println(letters.get(0) + " is a vowel");
+        }
+
     }
 
     public ArrayList<Integer> randomTenNums() {
         ArrayList<Integer> randomNums = new ArrayList<Integer>();
         Random randomNum = new Random();
 
-        for (int i=0; i<10; i++){
-            randomNums.add(randomNum.nextInt(100-55)+55);
+        for (int i = 0; i < 10; i++) {
+            randomNums.add(randomNum.nextInt(100 - 55) + 55);
         }
-        return(randomNums);
+        return (randomNums);
     }
 
     public ArrayList<Integer> returnSorted(ArrayList<Integer> myArray) {
         Collections.sort(myArray);
-        return(myArray);
+        return (myArray);
     }
 }
