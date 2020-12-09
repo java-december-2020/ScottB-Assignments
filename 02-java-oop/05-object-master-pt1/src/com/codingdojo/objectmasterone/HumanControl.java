26f002c2ef996505e11a80bc606c299e2e4b390d
Scott Bagley <scott@base-four.com>
2020-12-09 16:40:35 -0600

    Object Master 1

---
+++ 02-java-oop/05-object-master-pt1/src/com/codingdojo/objectmasterone/HumanControl.java
@@ -0,0 +1,10 @@
+package com.codingdojo.objectmasterone;
+
+public class HumanControl {
+	public static void main(String[] args) {
+		Human firstHuman = new Human();
+		Human attackHuman = new Human();
+		
+		firstHuman.attack(attackHuman);
+	}
+}
