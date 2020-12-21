13d17441aa460613aeff7e1352b84a4398f285ac
Scott Bagley <scott@base-four.com>
2020-12-21 13:53:05 -0600

    Routing V2

---
+++ 04-java-spring/01-spring-fundamentals/02-getting-familiar-with-routing/routingproject/src/main/java/com/scott/routingproject/DojoController.java
@@ -0,0 +1,26 @@
+package com.scott.routingproject;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DojoController {
+	@RequestMapping("/dojo")
+	public String dojo() {
+		return "The dojo is awesome";
+	}
+	
+	@RequestMapping("dojo/{location}")
+	public String dojoLocation(@PathVariable("location") String location) {
+		switch(location) {
+		
+		case "burbank":
+			return "Burbank Dojo is located in SoCal";
+		case "san-jose":
+			return "SJ dojo is headquarters";
+		default:
+			return String.format("%s Default",  location);
+		}
+	}
+}
